eb1c05c38b6d27b5c4aad26540febc9395800805
antonsablev <live.incntr@gmail.com>
2023-12-10 17:48:01 +0100

    add parent classes functionality

--- src/main/java/org/example/handler/RunParser.java
+++ src/main/java/org/example/handler/RunParser.java
@@ -1,14 +1,23 @@
 package org.example.handler;
 
+import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.example.service.CsvWriterService;
+import org.example.service.OwlClassesService;
+import org.example.service.OwlReader;
+
+import java.util.Set;
 
 public class RunParser {
     public void run(String readUri, String writeUri) {
         CreateOntModelHandler createOntModelHandler = new CreateOntModelHandler();
         OntModel model = createOntModelHandler.getOntologyModelByUrl(readUri);
+        DuplicateRemover duplicateRemover = new DuplicateRemover();
+        OwlReader reader = new OwlReader(model);
+        OwlClassesService classesService = new OwlClassesService(duplicateRemover, model);
+        CsvWriterService writerService = new CsvWriterService(reader, classesService);
 
-        CsvWriterService writerService = new CsvWriterService(model);
-        writerService.write(writeUri);
+        Set<OntClass> read = reader.read();
+        writerService.write(writeUri, read);
     }
 }
